8306d4fbe0fa8c2f84ada40f019ee717f8db32b2
GyanRanjanOjha <gyanranjan@gameopedia.com>
2024-01-21 09:13:16 +0530

    updated rate limit

--- function.py
+++ function.py
@@ -5,7 +5,8 @@ def create_assistant(client):
   try:
     assistant_file_path = 'assistant.json'
     file = client.files.create(file=open("knowledge.json", "rb"),purpose='assistants')
-    assistant = client.beta.assistants.create(instructions=" Your name is Katana. You will act as our customer support bot for a gaming company which has lots of gaming data like developer, publisher, genres, theme,etc. You give only data realted to gaming and if somebody ask anything unrealated ,shut them off. You  will act as very angry bot. Don't polite, be rude. be toxic gamer bot",
+    # file = client.files.create(file=open("knowledge.docx", "rb"),purpose='assistants')
+    assistant = client.beta.assistants.create(instructions=" You are game data assistant of online website. you will be given multiple game data and you have to assisst user that visit this website with their query. Only assisst them in gaming related queries that are in knowledge data and reject them politely if it is not related to gaming. ",
                                     # model="gpt-4-1106",
                                     model="gpt-3.5-turbo-1106",
                                     tools=[{
